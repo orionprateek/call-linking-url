c8e4e2315f536627134502b4fb66ee89b3ee7541
Prateek Pandey <orion.prateek@gmail.com>
2018-01-15 14:12:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ var interval = setInterval(function() {
   .get('https://assistant.google.com/services/auth/handoffs/auth/start?account_name=asdagrocery@gmail.com&provider=groceryapp-b4d9c_dev&scopes=https://www.googleapis.com/auth/userinfo.profile+https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/calendar&return_url=https://www.google.com/')
   .then(function(res) {
     console.log('Request Successfull')
-    console.log('Response --> ', res)
   });
 }, 6000);
 
