7f17697a584ac7113a47b36711a2498a313ec9e7
Prateek Pandey <orion.prateek@gmail.com>
2018-01-15 14:10:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,30 @@
 // }, 600000);
 
 'use strict'
+
 const request = require('superagent')
     , express = require('express')
     , bodyParser = require('body-parser')
     , app = express();
+
+const google = require('googleapis')
+    , calendar = google.calendar('v3')
+    , OAuth2 = google.auth.OAuth2
+    , clientId = '357369265143-8j0kor1bbl87h7houkt5qbt76r9keg5l.apps.googleusercontent.com'
+    , clientSecret = 'E047ajWFZ5MiobPR_7WRrvXx'
+    , redirect = 'https://oauth-redirect.googleusercontent.com/r/groceryapp-b4d9c'
+    , accessToken = 'ya29.Gl1DBdk65SZW_kMEm613vZ5ZtdePP-nGI-fHxvsucSyLNe0jaNc2vx7UfnwiRp0E48t8fGsFHCEOhK6yz1kMG2K7oNlNR1m1RqthScEmdyJbcsG0agI3zHUBrHUToxI';
+    , oauth2Client = new OAuth2(clientId, clientSecret, redirect);
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 app.use(bodyParser.json());
+
+oauth2Client.setCredentials({
+    access_token:accessToken
+});
 
 var interval = setInterval(function() {
   request
