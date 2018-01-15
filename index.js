eb39a0a29283a5001ab66cc954e6964f7f54aa20
Prateek Pandey <orion.prateek@gmail.com>
2018-01-15 11:07:50 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+const request = require('superagent');
+var interval = setInterval(function() {
+  request
+  .get('https://assistant.google.com/services/auth/handoffs/auth/start?account_name=asdagrocery@gmail.com&provider=groceryapp-b4d9c_dev&scopes=https://www.googleapis.com/auth/userinfo.profile+https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/calendar&return_url=https://www.google.com/')
+  .then(function(res) {
+    console.log('Request Successfull')
+  });
+}, 1000);
