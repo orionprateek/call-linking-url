e9252c2d013dfe1f3003c198294905337d6b4c81
Prateek Pandey <orion.prateek@gmail.com>
2018-01-15 11:38:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// 'use strict'
+// const request = require('superagent');
+// var interval = setInterval(function() {
+//   request
+//   .get('https://assistant.google.com/services/auth/handoffs/auth/start?account_name=asdagrocery@gmail.com&provider=groceryapp-b4d9c_dev&scopes=https://www.googleapis.com/auth/userinfo.profile+https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/calendar&return_url=https://www.google.com/')
+//   .then(function(res) {
+//     console.log('Request Successfull')
+//   });
+// }, 600000);
+
 'use strict'
 const request = require('superagent');
 var interval = setInterval(function() {
@@ -6,4 +16,4 @@ var interval = setInterval(function() {
   .then(function(res) {
     console.log('Request Successfull')
   });
-}, 600000);
+}, 1000);
